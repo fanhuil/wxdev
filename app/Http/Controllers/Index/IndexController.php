7862bf5c7db926cbf9a4ad54e6626134b4d1c3a7
fanhuilin <fanhuilinl@163.com>
2021-03-04 15:58:42 +0800

    no message

--- app/Http/Controllers/Index/IndexController.php
+++ app/Http/Controllers/Index/IndexController.php
@@ -4,39 +4,52 @@ namespace App\Http\Controllers\Index;
 
 use App\Http\Controllers\Controller;
 use App\Models\Article;
+use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     //
-    public function index()
+    public function index(Request $request)
     {
+        $string = $request->ip();
+        file_put_contents('ip.html', $string . "-", FILE_APPEND);
+        return $string;
+        $hotArticle = Article::where('is_hot', 1)->get();
+        return view('index.index', compact('hotArticle'));
 
-        $hotArticle = Article::where('is_hot',1)->get();
-        return view('index.index',compact('hotArticle'));
+
+        
+
+
     }
 
     # 顺序查找法
-    public function order_search(){
+    public function order_search()
+    {
 
     }
 
     # 二分查找法
-    public function binray_search(){
+    public function binray_search()
+    {
 
     }
 
     # 冒泡排序
-    public function bubble_sort(){
+    public function bubble_sort()
+    {
 
     }
 
     # 选择排序
-    public function select_sort(){
+    public function select_sort()
+    {
 
     }
 
     # 插入排序
-    public function insert_sort(){
+    public function insert_sort()
+    {
 
     }
 }
