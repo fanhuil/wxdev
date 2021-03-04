c38c388c6aed0b2db4622a8789d9524687aae9df
fanhuilin <fanhuilinl@163.com>
2021-03-04 18:31:01 +0800

    no message

--- public/index.php
+++ public/index.php
@@ -51,6 +51,8 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 */
 // App\Http\Kernel
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+dd($kernel);
+
 
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
